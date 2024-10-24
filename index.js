c909809ca1296f887bc3845d03fa7515084db104
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-10-24 14:22:01 +0530

    added delete function

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ connectMongoDB('mongodb://localhost:27017/learning-mongodb').then(()=>{
 //Middleware - plugin as we are using xxx-form-urlencoded 
 app.use(express.urlencoded({extended:false}));
 app.use(logReqRes('log.txt'));
-
-
 app.use('/api/users',userRouter);
+
 
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`);
